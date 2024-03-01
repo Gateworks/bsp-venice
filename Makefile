Gateworks/bsp-venice

commit 38445d029010e402718ecf868971db2a842caee5

added shell script for generating linux-headers
Added a shell script for generating linux-headers to be used for building
out of tree modules. Also moved the linux install directory out of the linux
kernel directory and renamed it 'build', in order for the script to avoid
recursion issues.

Added headers for building kernel modules to the linux
kernel install and tarball. The script is used in the Makefile in order
to populate the rootfs with these headers in /lib/modules/$KERNEL_RELEASE
, an example use case on a target board would be:

cryptodev-linux (simple - no deps):
apt update
apt install -y build-essential git bc file
git clone http://github.com/cryptodev-linux/cryptodev-linux
cd cryptodev-linux
make && make modules_install
modprobe cryptodev

This has an affect of adding 85M size increase on the target board for these
headers and also a 10M size increase for the jammy-venice.img.gz.

Signed-off-by: slee <slee@gateworks.com>

Makefile
@@ -122,46 +122,42 @@ endif
 kernel_image: linux-venice.tar.xz
 linux-venice.tar.xz: linux/arch/arm64/boot/Image venice-imx8mm-flash.bin
 	# install dir
-	rm -rf linux/install
-	mkdir -p linux/install/boot
+	rm -rf build/linux
+	mkdir -p build/linux/boot
 	# install uncompressed kernel
-	cp linux/arch/arm64/boot/Image linux/install/boot
+	cp linux/arch/arm64/boot/Image build/linux/boot
 	# install a compressed kernel in a kernel.itb
 	gzip -fk linux/arch/arm64/boot/Image
 	u-boot/tools/mkimage -f auto -A $(ARCH) \
 		-O linux -T kernel -C gzip \
 		-a $(LOADADDR) -e $(LOADADDR) -n "Kernel" \
-		-d linux/arch/arm64/boot/Image.gz linux/install/boot/kernel.itb
+		-d linux/arch/arm64/boot/Image.gz build/linux/boot/kernel.itb
 	# install dtbs
-	cp linux/arch/arm64/boot/dts/freescale/imx8*-venice-*.dtb* linux/install/boot
+	cp linux/arch/arm64/boot/dts/freescale/imx8*-venice-*.dtb* build/linux/boot
 	# install kernel modules
-	make -C linux INSTALL_MOD_STRIP=1 INSTALL_MOD_PATH=install modules_install
-	make -C linux INSTALL_HDR_PATH=install/usr headers_install
-	# cryptodev-linux build/install
-	make -C cryptodev-linux KERNEL_DIR=../linux
-	make -C cryptodev-linux KERNEL_DIR=../linux DESTDIR=../linux/install \
-		INSTALL_MOD_PATH=../linux/install install
-	# newracom nrc7292 802.11ah driver
-	make -C nrc7292/package/src/nrc/ KDIR=$(PWD)/linux modules
-	make -C nrc7292/package/src/nrc/ KDIR=$(PWD)/linux \
-		INSTALL_MOD_PATH=$(PWD)/linux/install modules_install
+	make -C linux INSTALL_MOD_STRIP=1 INSTALL_MOD_PATH=../build/linux modules_install
+	find build/linux/lib/modules/ -name build -exec rm {} \; # remove the bogus symlink
+	#install user space linux headers
+	make -C linux INSTALL_HDR_PATH=../build/linux/usr headers_install
 	# neramcom nrc7292 firmware
-	mkdir -p linux/install/lib/firmware
+	mkdir -p build/linux/lib/firmware
 	cp nrc7292/package/evk/sw_pkg/nrc_pkg/sw/firmware/nrc7292_* \
-		linux/install/lib/firmware/
+		build/linux/lib/firmware/
 	# newracom nrc7292 cli app
 	make CC=$(CROSS_COMPILE)gcc LFLAGS=-static -C nrc7292/package/src/cli_app/
-	mkdir -p linux/install/usr/local/bin/
+	mkdir -p build/linux/usr/local/bin/
 	cp nrc7292/package/src/cli_app/cli_app \
-		linux/install/usr/local/bin/
+		build/linux/usr/local/bin/
 	# FTDI USB-SPI driver
 	make -C ftdi-usb-spi \
-		KDIR=$(PWD)/linux INSTALL_MOD_PATH=$(PWD)/linux/install \
+		KDIR=$(PWD)/linux INSTALL_MOD_PATH=$(PWD)/build/linux \
 		INSTALL_MOD_STRIP=1 \
 		modules modules_install
+	# install kernel headers needed for building external modules ( aka linux-devel )
+	./venice/configure_kernel_headers.sh $(PWD)/linux $(PWD)/build/linux
 	# tarball
 	tar -cvJf linux-venice.tar.xz --numeric-owner --owner=0 --group=0 \
-		-C linux/install .
+		-C build/linux .
 
 # ubuntu
 PART_OFFSETMB ?= 16
@@ -208,7 +204,7 @@ clean:
 	make -C buildroot clean
 	rm -f venice-*-flash.bin
 	rm -f firmware-venice-*.bin
-	rm -rf linux/install
+	rm -rf build
 	rm -rf $(DDR_FIRMWARE_VER)*
 	rm -rf u-boot/lpddr4_pmu_*.bin
 	rm -rf linux-venice.tar.xz
@@ -224,5 +220,5 @@ distclean:
 	make -C buildroot distclean
 	rm -f venice-*-flash.bin
 	rm -f firmware-venice-*.bin
-	rm -rf linux/install
+	rm -rf build
 	rm -rf $(DDR_FIRMWARE_VER)

configure_kernel_headers.sh
@@ -1,0 +1,83 @@
+#!/bin/bash
+# linux-devel package creation (to generate headers etc needed to build kernel modules against)
+arch=$ARCH
+KDIR=$1
+DEST=$2
+KVER=$(cat $KDIR/include/config/kernel.release)
+builddir="$DEST/lib/modules/$KVER/build"
+
+echo "Creating Linux kernel header files from $KDIR into $builddir..."
+
+cd $KDIR
+echo "Installing build files..."
+install -Dt "$builddir" -m644 .config Makefile Module.symvers System.map vmlinux
+install -Dt "$builddir/kernel" -m644 kernel/Makefile
+install -Dt "$builddir/arch/arm64" -m644 arch/arm64/Makefile
+cp -t "$builddir" -a scripts
+
+# required when STACK_VALIDATION is enabled
+#install -Dt "$builddir/tools/objtool" tools/objtool/objtool
+
+# required when DEBUG_INFO_BTF_MODULES is enabled
+#install -Dt "$builddir/tools/bpf/resolve_btfids" tools/bpf/resolve_btfids/resolve_btfids
+
+echo "Installing headers..."
+cp -t "$builddir" -a include
+cp -t "$builddir/arch/arm64" -a arch/arm64/include
+install -Dt "$builddir/arch/arm64/kernel" -m644 arch/arm64/kernel/asm-offsets.s
+
+# install headers for various subsystems
+install -Dt "$builddir/drivers/md" -m644 drivers/md/*.h # software RAID
+install -Dt "$builddir/net/mac80211" -m644 net/mac80211/*.h # wifi
+#install -Dt "$builddir/drivers/media/i2c" -m644 drivers/media/i2c/msp3400-driver.h
+#install -Dt "$builddir/drivers/media/usb/dvb-usb" -m644 drivers/media/usb/dvb-usb/*.h
+#install -Dt "$builddir/drivers/media/dvb-frontends" -m644 drivers/media/dvb-frontends/*.h
+#install -Dt "$builddir/drivers/media/tuners" -m644 drivers/media/tuners/*.h
+#install -Dt "$builddir/drivers/iio/common/hid-sensors" -m644 drivers/iio/common/hid-sensors/*.h
+
+echo "Installing KConfig files..."
+find . -name 'Kconfig*' -exec install -Dm644 {} "$builddir/{}" \;
+
+echo "Removing unneeded architectures..."
+local arch
+for arch in "$builddir"/arch/*/; do
+  [[ $arch = */arm64/ ]] && continue
+  echo "Removing $(basename "$arch")"
+  rm -r "$arch"
+done
+
+echo "Removing documentation..."
+rm -r "$builddir/Documentation"
+
+echo "Removing broken symlinks..."
+find -L "$builddir" -type l -printf 'Removing %P\n' -delete
+
+echo "Removing loose objects..."
+find "$builddir" -type f -name '*.o' -printf 'Removing %P\n' -delete
+
+echo "Stripping build tools..."
+local file
+while read -rd '' file; do
+  case "$(file -Sib "$file")" in
+    application/x-sharedlib\;*)      # Libraries (.so)
+      strip -v $STRIP_SHARED "$file" ;;
+    application/x-archive\;*)        # Libraries (.a)
+      strip -v $STRIP_STATIC "$file" ;;
+    application/x-executable\;*)     # Binaries
+      strip -v $STRIP_BINARIES "$file" ;;
+    application/x-pie-executable\;*) # Relocatable binaries
+      strip -v $STRIP_SHARED "$file" ;;
+  esac
+done < <(find "$builddir" -type f -perm -u+x ! -name vmlinux -print0)
+
+
+rm $builddir/vmlinux
+
+if [ "$CROSS_COMPILE" ]; then
+    # Required compilation steps
+    echo "Cross-Compiling important build tools..."
+    (cd "$builddir/scripts/basic" && ${CROSS_COMPILE}gcc --static -o fixdep fixdep.c)
+    (cd "$builddir/scripts/mod" && ${CROSS_COMPILE}gcc --static -o modpost modpost.c sumversion.c file2alias.c)
+fi
+
+echo "Finished creating Linux-headers-gateworks"

commit 0d0943f1c7e0129862371be5d0c36d7b02b31a08

Makefile: add back nrc and cryptodev modules
Add back the nrc and cryptodev modules accidently removed.

Fixes: 38445d029010 ("added shell script for generating linux-headers")
Signed-off-by: Tim Harvey <tharvey@gateworks.com>

Makefile
@@ -140,9 +140,17 @@ linux-venice.tar.xz: linux/arch/arm64/boot/Image venice-imx8mm-flash.bin
 	# install kernel modules
 	make -C linux INSTALL_MOD_STRIP=1 INSTALL_MOD_PATH=../build/linux modules_install
 	find build/linux/lib/modules/ -name build -exec rm {} \; # remove the bogus symlink
-	#install user space linux headers
+	# install user space linux headers
 	make -C linux INSTALL_HDR_PATH=../build/linux/usr headers_install
-	# neramcom nrc7292 firmware
+	# cryptodev-linux build/install
+	make -C cryptodev-linux KERNEL_DIR=../linux
+	make -C cryptodev-linux KERNEL_DIR=../linux DESTDIR=../build/linux \
+		INSTALL_MOD_PATH=../build/linux install
+	# newracom nrc7292 802.11ah driver
+	make -C nrc7292/package/src/nrc/ KDIR=$(PWD)/linux modules
+	make -C nrc7292/package/src/nrc/ KDIR=$(PWD)/linux \
+		INSTALL_MOD_PATH=$(PWD)/build/linux modules_install
+	# newracom nrc7292 firmware
 	mkdir -p build/linux/lib/firmware
 	cp nrc7292/package/evk/sw_pkg/nrc_pkg/sw/firmware/nrc7292_* \
 		build/linux/lib/firmware/